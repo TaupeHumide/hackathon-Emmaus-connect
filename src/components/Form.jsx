fabc74b16dd7b896801b0aa01d5c7bfa4a84bd64
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-06-28 22:37:32 +0200

    wip added antutu index dropdown and storage values

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -42,6 +42,7 @@ export default function Form() {
     indiceAntutu: "",
     ponderation: "",
     showImeiNum: false,
+    storagePoints: 0,
   });
 
   return (
@@ -119,37 +120,77 @@ export default function Form() {
             });
           }}
         >
-          <option defaultValue="">Selectionnez la valeur de la RAM</option>
-          <option value="">1 Go</option>
-          <option value="">2 Go</option>
-          <option value="">3 Go</option>
-          <option value="">4 Go</option>
-          <option value="">6 Go</option>
-          <option value="">8 Go</option>
-          <option value="">12 Go</option>
-          <option value="">16 Go</option>
-          <option value="">32 Go</option>
-          <option value="">64 Go</option>
-          <option value="">96 Go</option>
+          <option defaultValue="">Sélectionnez la valeur de la RAM</option>
+          <option value="1 Go">1 Go</option>
+          <option value="2 Go">2 Go</option>
+          <option value="3 Go">3 Go</option>
+          <option value="4 Go">4 Go</option>
+          <option value="6 Go">6 Go</option>
+          <option value="8 Go">8 Go</option>
+          <option value="12 Go">12 Go</option>
+          <option value="16 Go">16 Go</option>
+          <option value="32 Go">32 Go</option>
+          <option value="64 Go">64 Go</option>
+          <option value="96 Go">96 Go</option>
         </select>
         <br />
         <label htmlFor="stockage">Stockage:</label> &nbsp;
-        <input
+        <select
           id="stockage"
           placeholder="stockage"
           type="text"
           name="stockage"
-          onChange={(e) => setData({ ...data, stockage: e.target.value })}
-        ></input>
+          onChange={(e) => {
+            const selectedStorage = e.target.value;
+            const storagePointsValue = storagePoints[selectedStorage];
+            setData({
+              ...data,
+              stockage: selectedStorage,
+              storagePoints: storagePointsValue,
+            });
+          }}
+        >
+          <option defaultValue="">Sélectionnez la mémoire de stockage</option>
+          <option value="16 Go">16 Go</option>
+          <option value="32 Go">32 Go</option>
+          <option value="64 Go">64 Go</option>
+          <option value="128 Go">128 Go</option>
+          <option value="256 Go">256 Go</option>
+          <option value="512 Go">512 Go</option>
+          <option value="1 To">1 To</option>
+          <option value="2 To">2 To</option>
+          <option value="4 To">4 To</option>
+          <option value="8 To">8 To</option>
+        </select>
         <br />
         <label htmlFor="indiceAntutu">Indice Antutu:</label> &nbsp;
-        <input
+        <select
           id="indiceAntutu"
-          placeholder="Indice_Antutu"
           type="text"
           name="indiceAntutu"
           onChange={(e) => setData({ ...data, indiceAntutu: e.target.value })}
-        ></input>
+        >
+          q<option defaultValue="">Sélectionnez un indice Antutu</option>
+          <option value="">0-50 000</option>
+          <option value="">50 000-100 000</option>
+          <option value="">100 000-150 000</option>
+          <option value="">150 000-200 000</option>
+          <option value="">250 000-300 000</option>
+          <option value="">300 000-350 000</option>
+          <option value="">350 000-400 000</option>
+          <option value="">400 000-450 000</option>
+          <option value="">450 000-500 000</option>
+          <option value="">500 000-550 000</option>
+          <option value="">550 000-600 000</option>
+          <option value="">600 000-650 000</option>
+          <option value="">650 000-700 000</option>
+          <option value="">700 000-750 000</option>
+          <option value="">750 000-800 000</option>
+          <option value="">800 000-850 000</option>
+          <option value="">850 000-900 000</option>
+          <option value="">900 000-950 000</option>
+          <option value="">950 000</option>
+        </select>
         <br />
         <label htmlFor="ponderation">Pondération:</label> &nbsp;
         <select
@@ -158,7 +199,7 @@ export default function Form() {
           name="ponderation"
           onChange={(e) => setData({ ...data, ponderation: e.target.value })}
         >
-          <option defaultValue="">Selectionnez une ponderation</option>
+          <option defaultValue="">Sélectionnez une pondération</option>
           <option value="">-100%</option>
           <option value="">-50%</option>
           <option value="">-10%</option>
