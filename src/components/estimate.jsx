4b6ee90ad1e3798deeeb9241f6177bc6d8cff2a4
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-06-28 23:56:03 +0200

    wip

--- src/components/estimate.jsx
+++ src/components/estimate.jsx
@@ -1,17 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import "./Estimate.css";
 
 function Estimate() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/form");
+  };
   return (
     <div className="estimate_main_div">
-      <h1 className="estimate_title">Estimation</h1>
+      <h1 className="estimate_title" onClick={handleClick}>
+        Estimation
+      </h1>
       <button className="estimate_button">
-        <img src="/desktop_icon.jpg" alt="" />
+        <img src="../../public/desktop_icon.jpg" alt="" />
       </button>
       <button className="estimate_button">
-        <img src="/tablet_icon.jpg" alt="" />
+        <img src="../../public/tablet_icon.jpg" alt="" />
       </button>
       <button className="estimate_button">
-        <img src="/phone_icon.jpg" alt="" />
+        <img src="../../public/phone_icon.jpg" alt="" />
       </button>
     </div>
   );
