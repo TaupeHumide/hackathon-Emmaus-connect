286b4e94191ad9e2dac9690e9d0bc8b29f2c3c6a
Waheb26 <cherwaheb@gmail.com>
2023-06-29 16:28:58 +0200

    css form & button2

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { RightOutlined, LeftOutlined } from "@ant-design/icons";
 import PropTypes from "prop-types";
+
 import "./Buttons.css";
 
 function Button({ onPrevious, onNext }) {
+  const handlePrevious = () => {
+    if (onPrevious) {
+      onPrevious();
+    }
+  };
+
+  const handleNext = () => {
+    if (onNext) {
+      onNext();
+    }
+  };
   return (
     <div className="button-container">
-      <button className="previous-button" onClick={onPrevious}>
-        Précédent
-      </button>
-      <button className="next-button" onClick={onNext}>
-        Suivant
-      </button>
+      <LeftOutlined className="fleche-gauche" onClick={handlePrevious} />
+      <RightOutlined className="fleche-droite" onClick={handleNext} />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -48,180 +48,12 @@ export default function Form() {
 
   return (
     <div>
+      <h1 className="Titre-Form">FORMULAIRE</h1>
       <Buttons className="Boutons" />
       <div className="container">
         <div className="form-container">
           <form className="input" onSubmit={handleSubmit}>
-            Do you have an imei number ? &nbsp;
-            <input
-              type="radio"
-              id="imei"
-              name="imeiOption"
-              value="Yes"
-              onChange={(e) =>
-                setData({ ...data, imei: e.target.value, showImeiNum: true })
-              }
-            ></input>
-<<<<<<< HEAD
-          </div>
-        ) : null}
-        <br />
-        <label htmlFor="marque">Marque:</label>
-        &nbsp;
-        <select
-          id="marque"
-          name="marque"
-          type="text"
-          onChange={(e) => setData({ ...data, marque: e.target.value })}
-        >
-          <option value="Apple">Apple</option>
-          <option value="Apple">Android</option>
-        </select>
-        <br />
-        <label htmlFor="model">Modèle:</label> &nbsp;
-        <input
-          id="model"
-          placeholder="Modèle"
-          type="text"
-          name="model"
-          onChange={(e) => setData({ ...data, model: e.target.value })}
-        ></input>
-        <br />
-        <label htmlFor="ram">RAM:</label> &nbsp;
-        <select
-          id="ram"
-          type="text"
-          name="ram"
-          onChange={(e) => {
-            const selectedRam = e.target.value;
-            const ramPointsValue = ramPoints[selectedRam];
-            setData({
-              ...data,
-              ram: selectedRam,
-              ramPoints: ramPointsValue,
-            });
-          }}
-        >
-          <option defaultValue="">Sélectionnez la valeur de la RAM</option>
-          <option value="1 Go">1 Go</option>
-          <option value="2 Go">2 Go</option>
-          <option value="3 Go">3 Go</option>
-          <option value="4 Go">4 Go</option>
-          <option value="6 Go">6 Go</option>
-          <option value="8 Go">8 Go</option>
-          <option value="12 Go">12 Go</option>
-          <option value="16 Go">16 Go</option>
-          <option value="32 Go">32 Go</option>
-          <option value="64 Go">64 Go</option>
-          <option value="96 Go">96 Go</option>
-        </select>
-        <br />
-        <label htmlFor="stockage">Stockage:</label> &nbsp;
-        <select
-          id="stockage"
-          placeholder="stockage"
-          type="text"
-          name="stockage"
-          onChange={(e) => {
-            const selectedStorage = e.target.value;
-            const storagePointsValue = storagePoints[selectedStorage];
-            setData({
-              ...data,
-              stockage: selectedStorage,
-              storagePoints: storagePointsValue,
-            });
-          }}
-        >
-          <option defaultValue="">Sélectionnez la mémoire de stockage</option>
-          <option value="16 Go">16 Go</option>
-          <option value="32 Go">32 Go</option>
-          <option value="64 Go">64 Go</option>
-          <option value="128 Go">128 Go</option>
-          <option value="256 Go">256 Go</option>
-          <option value="512 Go">512 Go</option>
-          <option value="1 To">1 To</option>
-          <option value="2 To">2 To</option>
-          <option value="4 To">4 To</option>
-          <option value="8 To">8 To</option>
-        </select>
-        <br />
-        <label htmlFor="indiceAntutu">Indice Antutu:</label> &nbsp;
-        <select
-          id="indiceAntutu"
-          type="text"
-          name="indiceAntutu"
-          onChange={(e) => setData({ ...data, indiceAntutu: e.target.value })}
-        >
-          q<option defaultValue="">Sélectionnez un indice Antutu</option>
-          <option value="">0-50 000</option>
-          <option value="">50 000-100 000</option>
-          <option value="">100 000-150 000</option>
-          <option value="">150 000-200 000</option>
-          <option value="">250 000-300 000</option>
-          <option value="">300 000-350 000</option>
-          <option value="">350 000-400 000</option>
-          <option value="">400 000-450 000</option>
-          <option value="">450 000-500 000</option>
-          <option value="">500 000-550 000</option>
-          <option value="">550 000-600 000</option>
-          <option value="">600 000-650 000</option>
-          <option value="">650 000-700 000</option>
-          <option value="">700 000-750 000</option>
-          <option value="">750 000-800 000</option>
-          <option value="">800 000-850 000</option>
-          <option value="">850 000-900 000</option>
-          <option value="">900 000-950 000</option>
-          <option value="">950 000</option>
-        </select>
-        <br />
-        <label htmlFor="ponderation">Pondération:</label> &nbsp;
-        <select
-          id="ponderation"
-          type="text"
-          name="ponderation"
-          onChange={(e) => setData({ ...data, ponderation: e.target.value })}
-        >
-          <option defaultValue="">Sélectionnez une pondération</option>
-          <option value="">-100%</option>
-          <option value="">-50%</option>
-          <option value="">-10%</option>
-          <option value="">-5%</option>
-          <option value="">0%</option>
-          <option value="">5%</option>
-          <option value="">10%</option>
-        </select>
-        <br />
-        <button type="submit">Valider</button>
-      </form>
-=======
-            <label htmlFor="ImeiYes"> Oui</label>
-            <input
-              id="imei"
-              type="radio"
-              value="No"
-              name="imeiOption"
-              onChange={(e) =>
-                setData({ ...data, imei: e.target.value, showImeiNum: false })
-              }
-            ></input>
-            <label htmlFor="ImeiNo"> Non</label>
-            <br />
-            {data.showImeiNum ? (
-              <div>
-                <label htmlFor="imeiNum">Numero Imei:</label> &nbsp;
-                <input
-                  id="imeiNum"
-                  placeholder="imeiNum"
-                  type="text"
-                  name="imeiNum"
-                  onChange={(e) =>
-                    setData({ ...data, imeiNum: e.target.value })
-                  }
-                ></input>
-              </div>
-            ) : null}
-            <br />
-            <label htmlFor="marque">Marque:</label>
+            <label htmlFor="marque">Marque</label>
             &nbsp;
             <select
               id="marque"
@@ -233,7 +65,7 @@ export default function Form() {
               <option value="Apple">Android</option>
             </select>
             <br />
-            <label htmlFor="model">Modèle:</label> &nbsp;
+            <label htmlFor="model">Modèle</label> &nbsp;
             <input
               id="model"
               placeholder="Modèle"
@@ -242,7 +74,7 @@ export default function Form() {
               onChange={(e) => setData({ ...data, model: e.target.value })}
             ></input>
             <br />
-            <label htmlFor="ram">RAM:</label> &nbsp;
+            <label htmlFor="ram">RAM</label> &nbsp;
             <select
               id="ram"
               type="text"
@@ -271,7 +103,7 @@ export default function Form() {
               <option value="">96 Go</option>
             </select>
             <br />
-            <label htmlFor="stockage">Stockage:</label> &nbsp;
+            <label htmlFor="stockage">Stockage</label> &nbsp;
             <input
               id="stockage"
               placeholder="stockage"
@@ -291,7 +123,7 @@ export default function Form() {
               }
             ></input>
             <br />
-            <label htmlFor="ponderation">Pondération:</label> &nbsp;
+            <label htmlFor="ponderation">Pondération</label> &nbsp;
             <select
               id="ponderation"
               type="text"
@@ -314,7 +146,6 @@ export default function Form() {
           </form>
         </div>
       </div>
->>>>>>> 5a000fc (css form & button)
     </div>
   );
 }
